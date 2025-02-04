62be2deee76b5abab03549ccb86f3e269ac6a6db
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-02-04 18:11:08 +0000

    Updated MPI build settings

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,13 @@ if [[ "$PLATFORM" == 'Darwin' ]]; then
   BUILD_OMP=OFF
 fi
 
+if [ "${mpi}" == "nompi" ]; then
+  ENABLE_MPI=OFF
+else
+  ENABLE_MPI=TRUE
+  export LDFLAGS="-lmpi ${LDFLAGS}"
+fi
+
 mkdir build && cd build
 
 cmake -DPKG_ML-METATENSOR=ON \
@@ -17,9 +24,17 @@ cmake -DPKG_ML-METATENSOR=ON \
       -DPKG_MC=OFF \
       -DPKG_MOLECULE=OFF \
       -DBUILD_OMP=$BUILD_OMP \
+      -DENABLE_MPI=$ENABLE_MPI \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       ../cmake
 
 cmake --build . --parallel ${CPU_COUNT} -- VERBOSE=1
 cmake --build . --target install
 
+cp lmp $PREFIX/bin/lmp
+
+if [ "${mpi}" == "nompi" ]; then
+  ln -s lmp ${PREFIX}/bin/lmp_serial
+else
+  ln -s lmp ${PREFIX}/bin/lmp_mpi
+fi
