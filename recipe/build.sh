e0d7d3ab38c53296a7ef030997ca2d98b535e856
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-03-19 13:31:56 +0000

    Added more packages to LAMMPS

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,10 @@ else
   fi
 fi
 
+# Parallel and library
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+
 if [ "${mpi}" == "nompi" ]; then
   ENABLE_MPI=OFF
 else
@@ -26,23 +30,34 @@ mkdir build && cd build
 cmake -DPKG_ML-METATENSOR=ON \
       -DLAMMPS_INSTALL_RPATH=ON \
       -DCMAKE_PREFIX_PATH="$TORCH_PREFIX" \
-      -DPKG_REPLICA=OFF \
-      -DPKG_PLUMED=OFF \
-      -DPKG_MC=OFF \
-      -DPKG_MOLECULE=OFF \
+      -DPKG_REPLICA=ON \
+      -DPKG_PLUMED=ON \
+      -DPLUMED_MODE=runtime \
+      -DPKG_MC=ON \
+      -DPKG_MOLECULE=ON \
+      -DPKG_MISC=ON \
+      -DPKG_KSPACE=ON \
+      -DPKG_MANIFOLD=ON \
+      -DPKG_QTB=ON \
+      -DPKG_REACTION=ON \
+      -DPKG_RIGID=ON \
+      -DPKG_SHOCK=ON \
+      -DPKG_SPIN=ON \
+      -DKG_VORONOI=ON \
+      -DEXTRA_PAIR=ON \
       -DBUILD_OMP=$BUILD_OMP \
       -DENABLE_MPI=$ENABLE_MPI \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+      -DCMAKE_INSTALL_RPATH=$PREFIX/lib \
       $args \
       ../cmake
 
 cmake --build . --parallel ${CPU_COUNT} -- VERBOSE=1
 cmake --build . --target install
 
-cp lmp $PREFIX/bin/lmp
-
 if [ "${mpi}" == "nompi" ]; then
-  ln -s lmp ${PREFIX}/bin/lmp_serial
+  cp lmp ${PREFIX}/bin/lmp_serial
 else
-  ln -s lmp ${PREFIX}/bin/lmp_mpi
+  cp lmp ${PREFIX}/bin/lmp_mpi
 fi
