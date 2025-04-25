1688862e30fe02724b4eaac75bda5592867c3b88
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-04-25 17:56:29 +0200

    Always link to libtorch, otherwise libtorch_cuda is never loaded

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,24 +11,27 @@ else
   #   CUDA_TOOLKIT_ROOT_DIR=$BUILD_PREFIX/targets/x86_64-linux
   #   args=$args" -DPKG_KOKKOS=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_CUDA=ON ${Kokkos_OPT_ARGS} -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_TOOLKIT_ROOT_DIR "
   # fi
-fi
 
-# Parallel and library
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+  # Make sure to link to `libtorch.so` and not just `libtorch_cpu.so`. This way,
+  # the code will try to load `libtorch_cuda.so` as well, enabling cuda device
+  # where available even when not using kokkos.
+  export LDFLAGS="-Wl,--no-as-needed,$PREFIX/lib/libtorch.so -Wl,--as-needed"
+fi
 
 if [ "${mpi}" == "nompi" ]; then
   ENABLE_MPI=OFF
 else
   ENABLE_MPI=TRUE
-  export LDFLAGS="-lmpi ${LDFLAGS}"
 fi
 
 
 mkdir build && cd build
 
-cmake -DLAMMPS_INSTALL_RPATH=ON \
-      -DCMAKE_PREFIX_PATH="$TORCH_PREFIX" \
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+      -DLAMMPS_INSTALL_RPATH=ON \
+      -DBUILD_OMP=$BUILD_OMP \
+      -DENABLE_MPI=$ENABLE_MPI \
       -DWITH_JPEG=OFF \
       -DWITH_PNG=OFF \
       -DPKG_REPLICA=ON \
@@ -49,19 +52,8 @@ cmake -DLAMMPS_INSTALL_RPATH=ON \
       -DPKG_SPIN=ON \
       -DPKG_MPIIO=$ENABLE_MPI \
       -DPKG_EXTRA_PAIR=ON \
-      -DBUILD_OMP=$BUILD_OMP \
-      -DENABLE_MPI=$ENABLE_MPI \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
-      -DCMAKE_INSTALL_RPATH=$PREFIX/lib \
       $args \
       ../cmake
 
 cmake --build . --parallel ${CPU_COUNT} -- VERBOSE=1
 cmake --build . --target install
-
-# if [ "${mpi}" == "nompi" ]; then
-#   cp lmp ${PREFIX}/bin/lmp_serial
-# else
-#   cp lmp ${PREFIX}/bin/lmp_mpi
-# fi
